2f2b939445044a43fa88f9aee9a2e94f994c7d4d
Mohamed Ali <79287488+Genius-code@users.noreply.github.com>
2021-10-01 15:02:18 +0200

    Update main js

--- js/main.js
+++ js/main.js
@@ -33,3 +33,22 @@ anime({
     targets: '.card-body',
     delay: anime.stagger(600, {easing: 'easeOutQuad'})
   });
+<!-- Initialize Swiper -->
+      var swiper = new Swiper(".swiper-container", {
+        effect: "coverflow",
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: "auto",
+        autoplay: {
+          delay: 2500,
+          disableOnInteraction: false,
+        },
+        coverflowEffect: {
+          rotate: 0,
+          stretch: 0,
+          depth: 40,
+          modifier: 1,
+          slideShadows: true,
+        },
+        loop: true,
+      });
